ca96499b120fd718ca0a7dcacdefbca5f6b2d60f
NatanSIsantos <55513395+NatanSIsantos@users.noreply.github.com>
2021-05-25 06:08:40 -0300

    Split nas entradas e convertendo para minúsculo.

--- bobbot/bobbot.pl
+++ bobbot/bobbot.pl
@@ -1,7 +1,7 @@
 bobbot:-
 	% Um pouco de suspense
 	write("Esperando o Bob se arrumar..."), nl,
-	sleep(2), nl,
+	sleep(1), nl,
 	
 	% Apresentação do Bob Bot
 	bobout("Desculpe a demora, eu sou Bob! "), 
@@ -18,15 +18,23 @@ bobout(Text):-
 	write(Text).
 
 % Entrada formatada paras frases do usuário
-usrin(S1):- 
+usrin(S1, S2, L):-
 	nl,
 	write("User:   "),
-	read_line_to_string(user_input, S1).
+	read_string(user_input,"\n", "\r\t ", _, S1),
+	% convertendo para letras minusculas 
+	string_lower(S1, S2),
+	% Realizando um split na string
+	split_string(S2, " , ", " ", L).
+
 
 % Loop onde a conversa entra o usuário e Bob é realizada
 conversation():-
-	usrin(S1),
-	S1=@="adeus", bobout("Adeus humano!");
+	usrin(_S1, _S2, L),
+	% printando strings a fim de teste. Apagar isso dps!
+	print(L),
+	% fim do fluxo de execução
+	L=@=["tchau"], bobout("Adeus humano!");
 	% A ideia é chamar as respostas no lugar do bobout abaixo.
 	bobout("Isso nao me significa nada"),
 	conversation().
